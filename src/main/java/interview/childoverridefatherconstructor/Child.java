dacdaa2bbf943c38de549f2add6b38ae9c1fb930
fansy <fansy1990@foxmail.com>
2019-01-04 12:48:07 +0800

    子类不能重载或重写父类构造函数

---
+++ src/main/java/interview/childoverridefatherconstructor/Child.java
@@ -0,0 +1,16 @@
+package interview.childoverridefatherconstructor;
+
+/**
+ * @Author: fansy
+ * @Time: 2019/1/4 12:45
+ * @Email: fansy1990@foxmail.com
+ */
+public class Child extends Father {
+//    public Father(){
+//
+//    }
+
+//    public Father(String a){
+//
+//    }
+}
---
+++ src/main/java/interview/childoverridefatherconstructor/Father.java
@@ -0,0 +1,12 @@
+package interview.childoverridefatherconstructor;
+
+/**
+ * @Author: fansy
+ * @Time: 2019/1/4 12:45
+ * @Email: fansy1990@foxmail.com
+ */
+public class Father {
+    public static void main(String[] args) {
+
+    }
+}
