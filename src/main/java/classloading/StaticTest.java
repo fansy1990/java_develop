77170395945c20a3c375e60b7b3ab4ea32bfda59
fansy <fansy1990@foxmail.com>
2019-03-06 10:56:45 +0800

    类加载机制

---
+++ src/main/java/classloading/StaticTest.java
@@ -0,0 +1,42 @@
+package classloading;
+
+/**
+ * @Author: fansy
+ * @Time: 2019/3/6 10:04
+ * @Email: fansy1990@foxmail.com
+ */
+public class StaticTest {
+    public static void main(String[] args) {
+        staticFunction();
+    }
+
+    static StaticTest st = new StaticTest();
+
+    static {   //静态代码块
+        System.out.println("1");
+    }
+
+    {       // 实例代码块
+        System.out.println("2");
+    }
+
+    StaticTest() {    // 实例构造器
+        System.out.println("3");
+        System.out.println("a=" + a + ",b=" + b);
+    }
+
+    public static void staticFunction() {   // 静态方法
+        System.out.println("4");
+    }
+
+    int a = 110;    // 实例变量
+    static int b = 112;     // 静态变量
+}
+
+/**
+ 2
+ 3
+ a=110,b=0
+ 1
+ 4
+ */
